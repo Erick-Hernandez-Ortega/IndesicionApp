c1ac8334adbf225d0fb78205edb946df2261edf7
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-01-20 20:04:27 -0600

    refactor: optimizar codigo de pruebas

--- tests/unit/components/counter.spec.ts
+++ tests/unit/components/counter.spec.ts
@@ -1,15 +1,20 @@
 import Counter from "@/components/Counter.vue";
-import { DOMWrapper, shallowMount } from '@vue/test-utils';  // Probar componentes de vue
+import {DOMWrapper, shallowMount, VueWrapper} from '@vue/test-utils';  // Probar componentes de vue
 
-describe('Counter view', () => { 
+describe('Counter view', () => {
+    let wrapper: VueWrapper<any>;
+
+    // es un hook que se ejecuta antes de cada prueba individual dentro de un bloque describe
+    beforeEach(() => {
+        wrapper = shallowMount(Counter);
+    })
+
     test('Debe de hacer match con el snapshot', () => { 
         // Al hacer esto por primera vez se crea un snapshot y se guarda
-        const wrapper = shallowMount(Counter);
         expect(wrapper.html()).toMatchSnapshot();
      });
 
     test('h2 debe mostar valor por defecto', async() => {
-        const wrapper = shallowMount(Counter);
         expect(wrapper.find("h2").exists()).toBeTruthy();
 
         const h2: DOMWrapper<HTMLHeadingElement> = wrapper.find('h2');
@@ -18,7 +23,6 @@ describe('Counter view', () => {
      })
 
     test('debe de incrementar en uno el valor del contador y decrementarlo', async () => {
-        const wrapper = shallowMount(Counter);
         const [increaseBTN, decreaseBTN] = wrapper.findAll('button');
 
         await increaseBTN.trigger('click');  // Esperamos a que el dom se actualice
